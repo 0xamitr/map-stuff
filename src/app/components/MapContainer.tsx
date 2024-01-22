e737fd244db6e61132df8a80cfcb7a68e7f53535
0xamitr <0xamitr@gmail.com>
2024-01-23 03:47:29 +0530

    modularity perhaps

--- src/app/components/MapContainer.tsx
+++ src/app/components/MapContainer.tsx
@@ -1,27 +1,30 @@
 "use client"
-import Map from './Map';
 import disputed from './dispute.json';
+import { useState } from 'react';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-const DEFAULT_CENTER = [20.5937, 78.9629]
-const MapContainer = () =>{
+const Map = () =>{
+  const DEFAULT_CENTER: [number, number] = [20.5937, 78.9629]
   const geoJsonStyle = {
     fillColor: 'transparent',
-    color: 'grey',
-    weight: 1,
+    color: 'lightgrey',
+    weight: 2,
   };
+
+  const [jsonObject, setJsonObject] = useState([]);
   return (
-        <Map center={DEFAULT_CENTER} minZoom={4} zoom={4}>
-        {({ TileLayer, GeoJSON, Popup }: any) => (
-            <>
+          <MapContainer style={{height:"60vh"}}center={DEFAULT_CENTER} zoom={4} minZoom={4}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             />
-            <GeoJSON key='disputed-geojson' data={disputed} style={geoJsonStyle}/>
-            </>
-        )}
-        </Map>
-  )
+              <GeoJSON key='disputed-geojson' data={disputed as any} style={geoJsonStyle}/>
+              {jsonObject.map((element, index)=>(   
+                <GeoJSON key='disputed-geojson' data={element} style={geoJsonStyle}/>
+              ))}
+          </MapContainer>
+        )
 }
 
-export default MapContainer;
+export default Map;
