67344ea76f2deb12a06329c20fdeeefbbdc1aaac
0xamitr <0xamitr@gmail.com>
2024-08-20 23:48:47 +0530

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 "use client"
-import Map from './components/map';
+import dynamic from 'next/dynamic';
+import 'leaflet/dist/leaflet.css';  // Import Leaflet CSS globally
+
+const Map = dynamic(() => import('./components/map'), {
+  ssr: false // This ensures that the component is only rendered on the client side
+});
 
 export default function Home() {
   return (
